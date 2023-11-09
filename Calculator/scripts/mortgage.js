6c5d29ee24a980e32fa428ed6d85da6e5f2848cb
lexiama <alexia.mckoy@gmail.com>
2023-11-09 13:44:49 -0500

    updated

--- Calculator/scripts/mortgage.js
+++ Calculator/scripts/mortgage.js
@@ -6,33 +6,50 @@ window.onload = init;
 function init () {
 	 // Button to submit form
     const calculateBtn = document.getElementById ("calculateBtn")
-    calculateBtn.onclick = calculateBtnclicked;
-     // convert Values to Number
-    const principal = Number(document.getElementById ("inputPrincipal").value);
-    const interestRate = Number(document.getElementById ("inputInterest").value);
-    const duration = Number(document.getElementById ("inputLoanLength").value);
-
-	//  Typeof helps see to dataTypes
-   console.log(principal)
-   console.log(typeof principal);
-   console.log (interestRate)
-   console.log (duration)
-   
+    calculateBtn.onclick = calculateBtnclicked;  
 };
 
 
-function 
+function calculateBtnclicked (){
+  // convert Values to Number
+  const principal = Number(document.getElementById ("inputPrincipal").value);
+  const interestRate = Number(document.getElementById ("inputInterest").value);
+  const duration = Number(document.getElementById ("inputLoanLength").value);
+  calculateMorgage (principal, interestRate, duration)
+
+  //  Typeof helps see to dataTypes
+ console.log(principal)
+ console.log(typeof principal);
+ console.log (interestRate)
+ console.log (duration)
+
+};
 
 function calculateMorgage (principal, interestRate, duration) {
-       
+    // step 1 convert the annual interest rate into monthly intreset rate and put into decimal notation
+		let monthlyInterestRate = (interestRate / 100) / 12; 
+	// step 2 convert the loan length from years to months
+		let loanLengthInMonths = duration * 12;
+	// step 3 monthly payment formula
+	let monthlyPayment = [principal *(monthlyInterestRate * (1 + monthlyInterestRate)**loanLengthInMonths)]
+												/[(1 + monthlyInterestRate)** loanLengthInMonths - 1];
+
+	// step 4 use monthly payment to calc total intrerest 
+	let totalInterest = (monthlyPayment * loanLengthInMonths) - principal;
+	// step 5 display output
+	displayOutput (monthlyPayment, totalInterest);
 };
 
-
- function displayOutput (monthlyPayment, interestCharged) {
-    const outputMonthlyPayment = document.getElementById ("outputMonthlyPayment")
-    const outputInterestPayed = document.getElementById ("outputTotalAmount")
+																// as long as its the same position the arguments
+																// will take the same paramter as long as it line up
+ function displayOutput (monthlyPayment, totalInterest) {
+    const outputMonthlyPayment = document.getElementById ("outputMonthlyPayment");
+    const outputInterestPayed = document.getElementById ("outputTotalAmount");
+	outputMonthlyPayment.value = monthlyPayment.toFixed(2);
+	outputInterestPayed.value = totalInterest.toFixed(2);
  
-    
+
+
 };
 
  
